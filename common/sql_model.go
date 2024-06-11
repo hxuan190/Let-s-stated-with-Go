72f994babd21ad32bac1d2488c6bb3b3bb9d4a4a
hxuan190 <hxuan190@gmail.com>
2024-06-11 21:56:41 +0700

    add uuid

--- common/sql_model.go
+++ common/sql_model.go
@@ -1,14 +1,28 @@
 package common
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
-type SqlModel struct {
-	Id        int        `gorm:"column:id" json:"id"`
+type SQLModel struct {
+	ID        uuid.UUID  `json:"id"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+}
+type SqlUserModel struct {
+	Id        uuid.UUID  `gorm:"column:id" json:"id"`
 	CreatedAt *time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt *time.Time `gorm:"column:updated_at" json:"updated_at,omitempty"`
 }
-type SqlUserModel struct {
-	Id        string     `gorm:"column:id" json:"id"`
-	CreatedAt *time.Time `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt *time.Time `gorm:"column:updated_at" json:"updated_at,omitempty"`
+
+func NewSQLModel() SQLModel {
+	id, _ := uuid.NewV7()
+	now := time.Now().UTC()
+
+	return SQLModel{
+		ID:        id,
+		CreatedAt: &now,
+		UpdatedAt: &now,
+	}
 }
