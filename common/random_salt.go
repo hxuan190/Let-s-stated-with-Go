424addc9c709fc371e735ba5ac2279d6ba62de9b
hxuan190 <hxuan190@gmail.com>
2024-06-13 00:29:48 +0700

    bcryptHasher

--- common/random_salt.go
+++ common/random_salt.go
@@ -2,14 +2,39 @@ package common
 
 import (
 	"crypto/rand"
-	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func GenerateRandomSalt(length int) (string, error) {
-	salt := make([]byte, length)
-	_, err := rand.Read(salt)
+type bcryptHasher struct{}
+
+func NewBcryptHasher() *bcryptHasher {
+	return &bcryptHasher{}
+}
+
+func (b *bcryptHasher) Hash(password, salt string) (string, error) {
+	str := fmt.Sprintf("%s.%s", password, salt)
+	hash, err := bcrypt.GenerateFromPassword([]byte(str), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
-	return base64.StdEncoding.EncodeToString(salt), nil
+
+	return string(hash), nil
+}
+
+func (b *bcryptHasher) Verify(password, salt, hash string) bool {
+	return password == hash
+}
+
+func (b *bcryptHasher) GenSalt(len int8) (string, error) {
+	var temp = make([]byte, len)
+
+	_, err := rand.Read(temp)
+
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(temp), nil
 }
